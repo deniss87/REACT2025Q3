fcf5b25226a6576b1a155fa70a009528193e2b18
Denis <me@email>
2025-08-31 21:51:18 +0300

    refactor: moved getFilteredData() to separate file

--- rs-react-app/src/components/CountryList.tsx
+++ rs-react-app/src/components/CountryList.tsx
@@ -1,8 +1,8 @@
 import { useMemo } from 'react';
 import { getCO2Resource } from '../utils/co2Resource';
 import CountryCard from './CountryCard';
-import { REGION_MAP } from '../utils/regionMap';
 import type { SortOption } from '../types/types';
+import { getFilteredData } from '../utils/filterCountries';
 
 interface Props {
   year: number;
@@ -22,41 +22,11 @@ export default function CountryList({
   const co2Resource = getCO2Resource();
   const data = co2Resource.read();
 
-  const filteredData = useMemo(() => {
-    return Object.entries(data)
-      .filter(([countryName, country]) => {
-        if (region !== 'All' && REGION_MAP[country.iso_code] !== region)
-          return false;
-        if (
-          searchQuery &&
-          !countryName.toLowerCase().includes(searchQuery.toLowerCase())
-        )
-          return false;
-        return true;
-      })
-      .sort(([aName, aCountry], [bName, bCountry]) => {
-        switch (sortOption) {
-          case 'name_asc':
-            return aName.localeCompare(bName);
-          case 'name_desc':
-            return bName.localeCompare(aName);
-          case 'population_asc': {
-            const aPop =
-              aCountry.data.find((d) => d.year === year)?.population ?? 0;
-            const bPop =
-              bCountry.data.find((d) => d.year === year)?.population ?? 0;
-            return aPop - bPop;
-          }
-          case 'population_desc': {
-            const aPop =
-              aCountry.data.find((d) => d.year === year)?.population ?? 0;
-            const bPop =
-              bCountry.data.find((d) => d.year === year)?.population ?? 0;
-            return bPop - aPop;
-          }
-        }
-      });
-  }, [data, region, searchQuery, sortOption, year]);
+  const filteredData = useMemo(
+    () => getFilteredData(data, year, region, searchQuery, sortOption),
+    [data, year, region, searchQuery, sortOption]
+  );
+
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
       {filteredData.map(([countryName, country]) => (
